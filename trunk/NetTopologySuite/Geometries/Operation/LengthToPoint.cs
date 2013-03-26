71eb596e75235bf9df9bc627b2da5b47facfa794
abhay.menon@gmail.com <abhay.menon@gmail.com@c8728489-9922-876f-7901-82ffbbbc8819>
2013-03-26 09:44:14 +0000

    First Level Additions

---
+++ trunk/NetTopologySuite/Geometries/Operation/LengthToPoint.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using GeoAPI.Geometries;
+
+namespace NetTopologySuite.Geometries.Operation
+{
+    /**
+    * Computes the length along a LineString to the point on the line nearest a given point.
+    */
+
+    internal class LengthToPoint
+    {
+        private double _locationLength;
+        private double _minDistanceToPoint;
+
+        public LengthToPoint(ILineString line, Coordinate inputPt)
+        {
+            ComputeLength(line, inputPt);
+        }
+
+        public static double LengthAlongSegment(LineSegment seg, Coordinate pt)
+        {
+            double projFactor = seg.ProjectionFactor(pt);
+            double len;
+            if (projFactor <= 0.0)
+                len = 0.0;
+            else if (projFactor <= 1.0)
+                len = projFactor*seg.Length;
+            else
+                len = seg.Length;
+            return len;
+        }
+
+        /**
+       * Computes the length along a LineString to the point on the line nearest a given point.
+       */
+
+        public static double Length(ILineString line, Coordinate inputPt)
+        {
+            var lp = new LengthToPoint(line, inputPt);
+            return lp.GetLength();
+        }
+
+        public double GetLength()
+        {
+            return _locationLength;
+        }
+
+        private void ComputeLength(ILineString line, Coordinate inputPt)
+        {
+            _minDistanceToPoint = Double.MaxValue;
+            double baseLocationDistance = 0.0;
+            Coordinate[] pts = line.Coordinates;
+            var seg = new LineSegment();
+            for (int i = 0; i < pts.Length - 1; i++)
+            {
+                seg.P0 = pts[i];
+                seg.P1 = pts[i + 1];
+                UpdateLength(seg, inputPt, baseLocationDistance);
+                baseLocationDistance += seg.Length;
+            }
+        }
+
+        private void UpdateLength(LineSegment seg, Coordinate inputPt, double segStartLocationDistance)
+        {
+            double dist = seg.Distance(inputPt);
+            if (dist > _minDistanceToPoint) return;
+            _minDistanceToPoint = dist;
+            // found new minimum, so compute location distance of point
+            double projFactor = seg.ProjectionFactor(inputPt);
+            if (projFactor <= 0.0)
+                _locationLength = segStartLocationDistance;
+            else if (projFactor <= 1.0)
+                _locationLength = segStartLocationDistance + projFactor*seg.Length;
+            else
+                _locationLength = segStartLocationDistance + seg.Length;
+        }
+    }
+}
---
+++ trunk/NetTopologySuite/Geometries/Operation/VertexHausdorffDistance.cs
@@ -0,0 +1,113 @@
+﻿using GeoAPI.Geometries;
+
+namespace NetTopologySuite.Geometries.Operation
+{
+    /**
+     * Implements algorithm for computing a distance metric
+     * which can be thought of as the "Vertex Hausdorff Distance".
+     * This is the Hausdorff distance restricted to vertices for
+     * one of the geometries.
+     * Also computes two points of the Geometries which are separated by the computed distance.
+     * <p>
+     * <b>NOTE: This algorithm does NOT compute the full Hausdorff distance correctly, but
+     * an approximation that is correct for a large subset of useful cases.
+     * One important part of this subset is Linestrings that are roughly parallel to each other,
+     * and roughly equal in length - just what is needed for line matching.
+     * </b>
+     */
+
+    public class VertexHausdorffDistance
+    {
+        private readonly PointPairDistance _ptDist = new PointPairDistance();
+
+        public VertexHausdorffDistance(IGeometry g0, IGeometry g1)
+        {
+            Compute(g0, g1);
+        }
+
+        public VertexHausdorffDistance(LineSegment seg0, LineSegment seg1)
+        {
+            Compute(seg0, seg1);
+        }
+
+        public Coordinate[] Coordinates
+        {
+            get { return _ptDist.GetCoordinates(); }
+        }
+
+        public static double Distance(IGeometry g0, IGeometry g1)
+        {
+            var vhd = new VertexHausdorffDistance(g0, g1);
+            return vhd.Distance();
+        }
+
+        public double Distance()
+        {
+            return _ptDist.GetDistance();
+        }
+
+        private void Compute(LineSegment seg0, LineSegment seg1)
+        {
+            computeMaxPointDistance(seg0, seg1, _ptDist);
+            computeMaxPointDistance(seg1, seg0, _ptDist);
+        }
+
+        /**
+         * Computes the maximum oriented distance between two line segments,
+         * as well as the point pair separated by that distance.
+         *
+         * @param seg0 the line segment containing the furthest point
+         * @param seg1 the line segment containing the closest point
+         * @param ptDist the point pair and distance to be updated
+         */
+
+        private void computeMaxPointDistance(LineSegment seg0, LineSegment seg1, PointPairDistance ptDist)
+        {
+            Coordinate closestPt0 = seg0.ClosestPoint(seg1.P0);
+            ptDist.SetMaximum(closestPt0, seg1.P0);
+            Coordinate closestPt1 = seg0.ClosestPoint(seg1.P1);
+            ptDist.SetMaximum(closestPt1, seg1.P1);
+        }
+
+        private void Compute(IGeometry g0, IGeometry g1)
+        {
+            computeMaxPointDistance(g0, g1, _ptDist);
+            computeMaxPointDistance(g1, g0, _ptDist);
+        }
+
+        private void computeMaxPointDistance(IGeometry pointGeom, IGeometry geom, PointPairDistance ptDist)
+        {
+            var distFilter = new MaxPointDistanceFilter(geom);
+            pointGeom.Apply(distFilter);
+            ptDist.SetMaximum(distFilter.GetMaxPointDistance());
+        }
+
+        public class MaxPointDistanceFilter : ICoordinateFilter
+        {
+            private readonly IGeometry _geom;
+            private readonly PointPairDistance _maxPtDist = new PointPairDistance();
+            private readonly PointPairDistance _minPtDist = new PointPairDistance();
+
+            public MaxPointDistanceFilter(IGeometry geom)
+            {
+                _geom = geom;
+            }
+
+            public PointPairDistance GetMaxPointDistance()
+            {
+                return _maxPtDist;
+            }
+
+            #region ICoordinateFilter Members
+
+            public void Filter(Coordinate coord)
+            {
+                _minPtDist.Initialize();
+                EuclideanDistanceToPoint.ComputeDistance(_geom, coord, _minPtDist);
+                _maxPtDist.SetMaximum(_minPtDist);
+            }
+
+            #endregion
+        }
+    }
+}
